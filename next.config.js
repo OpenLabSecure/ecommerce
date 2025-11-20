76f3d225b71bf2da97fb42d7d8bfc6fef9e37ca4
Maximo Omar <63993140+OMARROJASP@users.noreply.github.com>
2025-11-20 11:21:49 -0500

    feature/ cambios en la optimizacion de las imagenes

--- next.config.js
+++ next.config.js
@@ -62,6 +62,11 @@ const nextConfig = {
       // (Si usas también los public images de Medusa)
       { protocol: "https", hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com", pathname: "/**" },
     ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/webp'], // Usar WebP para mejor compresión
+    minimumCacheTTL: 60, // Cache de 60 segundos
+
   },
 }
 
